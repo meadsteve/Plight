d8a40961140e5da4744e38833b4d2bd4295a2401
steve b <meadsteve@gmail.com>
2015-02-01 17:55:16 +0000

    add temporary routes

--- plight/lib/plight/mock_routes.ex
+++ plight/lib/plight/mock_routes.ex
@@ -7,6 +7,27 @@ defmodule Plight.MockRoutes do
 
   def add(server, route, response) do
     GenServer.cast(server, {:add, route, response})
+    server
+  end
+
+  def add(server, route, response, [remove_in_minutes: timeout]) do
+    add(server, route, response, [remove_in_seconds: timeout * 60])
+  end
+
+  def add(server, route, response, [remove_in_seconds: timeout]) do
+    GenServer.cast(server, {:add, route, response})
+    spawn fn ->
+      IO.puts "Queing route #{route} for removal in #{timeout} seconds"
+      :timer.sleep(1000 * timeout)
+      IO.puts "removing route #{route} as timeout has expired"
+      Plight.MockRoutes.remove(server, route)
+    end
+    server
+  end
+
+  def remove(server, route) do
+    GenServer.cast(server, {:remove, route})
+    server
   end
 
   def lookup(server, route) do
--- plight/lib/plight/mocks/route_server.ex
+++ plight/lib/plight/mocks/route_server.ex
@@ -3,19 +3,25 @@ defmodule Plight.Mocks.RouteServer do
 
   ## Gen server hooks
   def init(:ok) do
-    {:ok, []}
+    {:ok, HashDict.new}
   end
 
   def handle_call({:lookup, route}, _from, routes) do
-    # TODO: look up routes
-    {:reply, {404, "No match for the given route"}, routes}
+    if (routes |> HashDict.has_key? route) do
+      {:ok, {code, message}} = HashDict.fetch(routes, route)
+      {:reply, {code, message}, routes}
+    else
+      {:reply, {404, "No match for the given route"}, routes}
+    end
   end
 
   def handle_cast({:add, route, response}, routes) do
-    #TODO: add route
-    {:noreply, routes}
+    {:noreply, routes |> HashDict.put(route, response)}
   end
 
-  # Domain logic
+  def handle_cast({:remove, route}, routes) do
+    {_response, updated_routes} = routes |> HashDict.pop(route)
+    {:noreply, updated_routes}
+  end
 
 end
